e3f918e2e06bac4b39a20447142a16e08a066749
Jonazhino <Jonazhino@DESKTOP-32LSEPT>
2025-05-24 11:28:47 -0500

    Update

--- src/config/dbConfig.cjs
+++ src/config/dbConfig.cjs
@@ -1,9 +1,17 @@
 
+// const dbConfig = {
+//   host: 'sql10.freesqldatabase.com',
+//   user: 'sql10780136', 
+//   password: 'WhVIT2MEuc',
+//   database: 'sql10780136',
+//   port: 3306
+// };
+
 const dbConfig = {
-  host: 'sql10.freesqldatabase.com',
-  user: 'sql10780136', 
-  password: 'WhVIT2MEuc',
-  database: 'sql10780136',
+  host: 'sql.freedb.tech',
+  user: 'freedb_iesrfa', 
+  password: 'XeRx*8Q*cCU@A9V',
+  database: 'freedb_iesrfa',
   port: 3306
 };
 
