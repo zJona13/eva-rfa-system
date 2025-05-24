ae4085b0f7c840e62c326ac213a7539a51da113a
Jonazhino <Jonazhino@DESKTOP-32LSEPT>
2025-05-24 11:47:18 -0500

    De nuevo

--- src/config/dbConfig.cjs
+++ src/config/dbConfig.cjs
@@ -1,18 +1,18 @@
 
-// const dbConfig = {
-//   host: 'sql10.freesqldatabase.com',
-//   user: 'sql10780136', 
-//   password: 'WhVIT2MEuc',
-//   database: 'sql10780136',
-//   port: 3306
-// };
-
 const dbConfig = {
-  host: 'sql.freedb.tech',
-  user: 'freedb_iesrfa', 
-  password: 'XeRx*8Q*cCU@A9V',
-  database: 'freedb_iesrfa',
+  host: 'sql10.freesqldatabase.com',
+  user: 'sql10780136', 
+  password: 'WhVIT2MEuc',
+  database: 'sql10780136',
   port: 3306
 };
+
+// const dbConfig = {
+//   host: 'sql.freedb.tech',
+//   user: 'freedb_iesrfa', 
+//   password: 'XeRx*8Q*cCU@A9V',
+//   database: 'freedb_iesrfa',
+//   port: 3306
+// };
 
 module.exports = dbConfig;
