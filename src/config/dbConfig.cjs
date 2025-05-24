60583d16e11c9e5935108d5a2389b521e428c169
Jonazhino <Jonazhino@DESKTOP-32LSEPT>
2025-05-24 12:43:14 -0500

    Update

--- src/config/dbConfig.cjs
+++ src/config/dbConfig.cjs
@@ -7,12 +7,4 @@ const dbConfig = {
   port: 3306
 };
 
-// const dbConfig = {
-//   host: 'sql.freedb.tech',
-//   user: 'freedb_iesrfa', 
-//   password: 'XeRx*8Q*cCU@A9V',
-//   database: 'freedb_iesrfa',
-//   port: 3306
-// };
-
 module.exports = dbConfig;
